c1c4452bfaeab6f35fc2f38930dc3928c6600cba
DumDumGeniuss <dumdumgenius@gmail.com>
2017-06-12 17:53:55 +0800

    Fix error on _document

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
               text-decoration: none;
             }
           `}</style>
-          <script>
+          <script>{`
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -34,8 +34,7 @@ export default class MyDocument extends Document {
           
             ga('create', 'UA-82279826-3', 'auto');
             ga('send', 'pageview');
-          
-          </script>
+          `}</script>
           <script src="https://apis.google.com/js/platform.js"></script>
         </Head>
         <body className="custom_class">
