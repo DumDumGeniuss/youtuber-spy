4a48b04e42759db8360c97b27fa13e221ee62fe4
DumDumGeniuss <dumdumgenius@gmail.com>
2017-06-12 18:02:10 +0800

    Change GA script position

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,15 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <script>{`
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          
+            ga('create', 'UA-82279826-3', 'auto');
+            ga('send', 'pageview');
+          `}</script>
           <title>YoutuberSpy</title>
           <link rel="icon" href="static/logo.png" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -26,15 +35,6 @@ export default class MyDocument extends Document {
               text-decoration: none;
             }
           `}</style>
-          <script>{`
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-          
-            ga('create', 'UA-82279826-3', 'auto');
-            ga('send', 'pageview');
-          `}</script>
           <script src="https://apis.google.com/js/platform.js"></script>
         </Head>
         <body className="custom_class">
