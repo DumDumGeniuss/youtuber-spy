0ad0bb0e62bdff4d3e687647bde9d10987188ea5
DumDumGeniuss <dumdumgenius@gmail.com>
2017-06-16 21:22:22 +0800

    Change oauth2 google scope

--- apis/youtube.js
+++ apis/youtube.js
@@ -6,7 +6,7 @@ import * as tinyHelper from '../libs/tinyHelper';
 const getUserInfoApi = 'https://www.googleapis.com/oauth2/v1/userinfo';
 const oauthApiurl = 'https://accounts.google.com/o/oauth2/v2/auth';
 const clientId = config.client_id;
-const scope = 'https://www.googleapis.com/auth/youtube.readonly';
+const scope = 'https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
 const response_type = 'token';
 const include_granted_scopes = 'true';
 
