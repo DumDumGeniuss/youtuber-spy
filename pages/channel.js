7f61103a16fa9b13e61b9fa49e7b7a167be0971c
DumDumGeniuss <dumdumgenius@gmail.com>
2017-06-24 21:52:18 +0800

    change platform.js position again

--- pages/channel.js
+++ pages/channel.js
@@ -100,10 +100,10 @@ class Index extends React.Component {
               <div className={'Channel-decorateZone'}>
               </div>
               <div className={'Channel-subscriber'}>
+                <script src="https://apis.google.com/js/platform.js"></script>
                 <div
                   className={'g-ytsubscribe'}
                   data-channelid={channelInfo._id} data-layout="default" data-count="default"></div>
-                <script src="https://apis.google.com/js/platform.js"></script>
               </div>
             </div>
             <h1 className={'Channel-title'}>{channelInfo.title}</h1>
