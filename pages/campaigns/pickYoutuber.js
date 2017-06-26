c10f5dfe9e4c0e1c82f622b50f42f1567d0f18d7
DumDumGeniuss <dumdumgenius@gmail.com>
2017-06-26 21:07:43 +0800

    Add new og tag

--- pages/campaigns/pickYoutuber.js
+++ pages/campaigns/pickYoutuber.js
@@ -48,7 +48,11 @@ class PickYoutuber extends React.Component {
           <meta name="og:description" content={pickedYoutuber.description}
           />
           <meta name="og:type" content="website" />
-          <meta name="og:url" content={'https://www.youtuberspy.com/campaigns/pickYoutuber?channelTitle=' + pickedYoutuber.title} />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="400" />
+          <meta property="og:image:height" content="400" />
+          <meta property="og:image" content={pickedYoutuber.highThumbnails} />
+          <meta property="og:url" content={'https://www.youtuberspy.com/campaigns/pickYoutuber?channelTitle=' + pickedYoutuber.title} />
           <meta property="og:site_name" content="小頻道大世界 - 在這裡發掘您喜歡的Youtubers！"/>
           <meta property="fb:app_id" content={'158925374651334'} />
         </Head>
