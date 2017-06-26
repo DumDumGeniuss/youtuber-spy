11e475d7134be49063470fa79c3063a5b932f4c0
DumDumGeniuss <dumdumgenius@gmail.com>
2017-06-26 18:29:18 +0800

    Change fb library position

--- pages/_document.js
+++ pages/_document.js
@@ -44,6 +44,15 @@ export default class MyDocument extends Document {
           {this.props.customValue}
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{__html: `
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=158925374651334";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `}} />
         </body>
       </html>
     );
