048dbbfc4f6f3ada7ca9eaa044b1269cedaa2d2f
DumDumGeniuss <dumdumgenius@gmail.com>
2017-07-22 02:21:04 +0800

    Fix the UI bug on singleChannel Page

--- pages/channels/singleChannel.js
+++ pages/channels/singleChannel.js
@@ -171,7 +171,7 @@ class SingleChannel extends React.Component {
                     return (
                       <div key={index} className={'SingleChannel-socialList'}>
                         <img className={'SingleChannel-socialIcon'} src={socialInfo.img} />
-                        <a className={'SingleChannel-socialTitle'} href={socialInfo.href}>{socialInfo.title}</a>
+                        <a target="_blank" className={'SingleChannel-socialTitle'} href={socialInfo.href}>{socialInfo.title}</a>
                       </div>
                     );
                   })
