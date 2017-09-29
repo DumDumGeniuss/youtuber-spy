1bc3ae6bf7bc6ca031f7a1d608f96682e67a407d
DumDumGeniuss <dumdumgenius@gmail.com>
2017-09-29 16:12:11 +1000

    Fix the bug on youtuber channel card

--- components/cards/YoutuberChannelCard/YoutuberChannelCard.js
+++ components/cards/YoutuberChannelCard/YoutuberChannelCard.js
@@ -25,9 +25,15 @@ class YoutuberChannelCard extends React.Component {
             <figure className={'YoutuberChannelCard-img'}><img alt={`youtuber channel ${channelInfo.title}`} src={channelInfo.defaultThumbnails} /></figure>
             <h2 className={'YoutuberChannelCard-title'}><b>{`${channelInfo.rank}.${channelInfo.title}`}</b></h2>
             <div className={'YoutuberChannelCard-dataZone'}>
-              <h2 className={'YoutuberChannelCard-data'}><small>訂閱 {channelInfo.subscriberCount.toLocaleString()}</small></h2>
-              <h2 className={'YoutuberChannelCard-data'}><small>影片 {channelInfo.videoCount.toLocaleString()}</small></h2>
-              <h2 className={'YoutuberChannelCard-data'}><small>觀看 {channelInfo.viewCount.toLocaleString()}</small></h2>
+              <h2 className={'YoutuberChannelCard-data'}><small>
+                訂閱 {channelInfo.subscriberCount && channelInfo.subscriberCount.toLocaleString()}
+              </small></h2>
+              <h2 className={'YoutuberChannelCard-data'}><small>
+                影片 {channelInfo.videoCount && channelInfo.videoCount.toLocaleString()}
+              </small></h2>
+              <h2 className={'YoutuberChannelCard-data'}><small>
+                觀看 {channelInfo.viewCount && channelInfo.viewCount.toLocaleString()}
+              </small></h2>
               <h2 className={'YoutuberChannelCard-data'}><small>種類 {channelInfo.category || '無'}</small></h2>
               <h2 className={'YoutuberChannelCard-data'}><small>地區 {channelInfo.country || '無'}</small></h2>
               <h2 className={'YoutuberChannelCard-data'}><small>成立 {`${parseInt(month / 12, 10)}年${month % 12}個月`}</small></h2>
